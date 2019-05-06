c719a109f091b40f49710a81420068a2295bd63c
WaltXin <xiongx@tcd,ie>
2019-05-06 13:54:22 +0100

    Add statistic as concreate obverser and subsribe concreate subject

--- src/Observer/Demo.java
+++ src/Observer/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         weatherData.setWeatherData(20.2f, 42.2f, 15.0f);
+        weatherData.setWeatherData(10.2f, 21.2f, 15.0f);
     }
 }
--- src/Observer/StatisticsDisplay.java
+++ src/Observer/StatisticsDisplay.java
@@ -5,13 +5,36 @@ package Observer;
  * */
 public class StatisticsDisplay implements Observer, Display{
 
+    int updateCount;
+    float temp;
+    float humidity;
+    float pressure;
+    Subject weatherData;
+
+    StatisticsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+        updateCount = 0;
+        temp = 0;
+        humidity = 0;
+        pressure = 0;
+    }
+
     @Override
     public void update(float temp, float humidity, float pressure) {
-
+        updateCount++;
+        this.temp += temp;
+        this.temp /= updateCount;
+        this.humidity += humidity;
+        this.humidity /= updateCount;
+        this.pressure += pressure;
+        this.pressure /= updateCount;
+        display();
     }
 
     @Override
     public void display() {
-
+        System.out.println("statistics display:  avg temperature: " +
+                String.format("%.2f", temp) + ", humidity: " + String.format("%.2f", humidity) + ", pressure: " + String.format("%.2f", pressure));
     }
 }
